6c2994fbcf5737d6641578af60ab0b319dd28e7c
Henriquesupiproramming <henriquesupiifc@gmail.com>
2022-05-06 11:02:43 -0300

    1

--- mydb.js
+++ mydb.js
@@ -3,10 +3,14 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "aluno01"
+  password: "aluno01 "
 });
 
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
+  con.query("CREATE DATABASE mydb", function (err, result) {
+    if (err) throw err;
+    console.log("Database created");
+  });
 });
